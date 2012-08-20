eff92d882c5816117bacc7d6d1fce896deccd735
Alden Torres <aldenml@gmail.com>
2012-08-20 10:02:48 -0400

    Header changes.

--- src/demo/BrowserFrame.java
+++ src/demo/BrowserFrame.java
@@ -1,6 +1,6 @@
 /*
  * Created by Alden Torres (aldenml)
- * Copyright (c) 2011, 2012, FrostWire(TM). All rights reserved.
+ * Copyright (c) 2011, 2012, FrostWire(R). All rights reserved.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
